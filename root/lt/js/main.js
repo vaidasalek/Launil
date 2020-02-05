0c98b62ec6978f78ddfdb93848ed8678992b3167
vaidasalek <57872113+vaidasalek@users.noreply.github.com>
2020-02-05 17:39:48 +0200

    Commit

--- root/lt/js/main.js
+++ root/lt/js/main.js
@@ -8,4 +8,26 @@ var currentScrollPos = window.pageYOffset;
 	  document.getElementById("header-bar-hide-after-scroll").style.top = "-130px";
   }
   prevScrollpos = currentScrollPos;
-}
+};
+
+
+$(document).ready(function() {
+    $(".navigation-list > li > a").click(function (e) { // binding onclick
+        if ($(this).parent().hasClass('selected')) {
+            $(".navigation-list .selected div div").slideUp(100); // hiding popups
+            $(".navigation-list .selected").removeClass("selected");
+        } else {
+            $(".navigation-list .selected div div").slideUp(100); // hiding popups
+            $(".navigation-list .selected").removeClass("selected");
+            if ($(this).next(".subs").length) {
+                $(this).parent().addClass("selected"); // display popup
+                $(this).next(".subs").children().slideDown(200);
+            }
+        }
+        e.stopPropagation();
+    });
+    $("body").click(function () { // binding onclick to body
+        $(".navigation-list .selected div div").slideUp(100); // hiding popups
+        $(".navigation-list .selected").removeClass("selected");
+    });
+});
